a2f2a36facdc68715d86a2d0aab8da86a4dcb317
hiromasa <howzheng0629@gmail.com>
2023-03-23 20:02:33 +0800

    db seed

--- backend/app/src/app.controller.ts
+++ backend/app/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { User } from '@prisma/client';
+import { AppService, UserService } from './app.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+		private readonly appService: AppService,
+		private readonly userService: UserService,
+	) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+	
+	@Get("/user")
+	getUsers(): Promise<User> {
+		return this.userService.getUser();
+	}
 }
--- backend/app/src/app.service.ts
+++ backend/app/src/app.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient, User } from '@prisma/client'
+const prisma = new PrismaClient()
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+}
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+	async onModuleInit() {
+		await this.$connect();
+	}
+	
+	async enableShutdownHooks(app: INestApplication) {
+		this.$on('beforeExit', async () => {
+			await app.close();
+		})
+	}
+}
+
+@Injectable()
+export class UserService {
+	constructor(private prisma: PrismaService) {}
+	
+	async getUser() {
+		return await this.prisma.user.findFirst();
+	}
 }
