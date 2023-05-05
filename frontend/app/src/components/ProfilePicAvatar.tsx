f3a28ce4b9390a537de350a2ef1ac4cc6360b609
Eason Chai <echai2905@gmail.com>
2023-05-05 19:07:53 +0800

    fixes

--- frontend/app/src/components/ProfilePicAvatar.tsx
+++ frontend/app/src/components/ProfilePicAvatar.tsx
@@ -1,21 +1,27 @@
-import { User } from '@prisma/client'
-import { Avatar } from 'antd'
-import React from 'react'
+import { User } from "@prisma/client";
+import { Avatar } from "antd";
+import React from "react";
 
 interface ProfilePicAvatarProps {
-	user: User
+  user: User;
 }
 
 const ProfilePicAvatar = (props: ProfilePicAvatarProps) => {
-	return (
-		<Avatar src={
-			props.user.image 
-			? props.user.image.substring(0, 4) === 'http' 
-				? props.user.image 
-				: `${process.env.NEXT_PUBLIC_NESTJS_URL}/users/image/${props.user.id}` 
-			: `https://source.boringavatars.com/pixel/150/${(new Date()).getTime()}`
-		} />
-	)
-}
+  return (
+    props &&
+    props.user &&
+    props.user.image && (
+      <Avatar
+        src={
+          props.user.image
+            ? props.user.image.substring(0, 4) === "http"
+              ? props.user.image
+              : `${process.env.NEXT_PUBLIC_NESTJS_URL}/users/image/${props.user.id}`
+            : `https://source.boringavatars.com/pixel/150/${new Date().getTime()}`
+        }
+      />
+    )
+  );
+};
 
-export default ProfilePicAvatar
+export default ProfilePicAvatar;
